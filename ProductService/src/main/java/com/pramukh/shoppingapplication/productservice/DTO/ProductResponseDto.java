92096678c32359112a1f9c8f7b7c8c6a9c7e2c39
Pramukh Prakash <pramukhp35@gmail.com>
2025-03-20 00:17:01 +0100

    feat(product) adding test cases and updating end points

--- ProductService/src/main/java/com/pramukh/shoppingapplication/productservice/DTO/ProductResponseDto.java
+++ ProductService/src/main/java/com/pramukh/shoppingapplication/productservice/DTO/ProductResponseDto.java
@@ -2,5 +2,5 @@ package com.pramukh.shoppingapplication.productservice.DTO;
 
 import java.math.BigDecimal;
 
-public record ProductResponseDto(String id, String name, String description, BigDecimal price) {
+public record ProductResponseDto(String id, String productName, String productDescription, BigDecimal productPrice) {
 }
