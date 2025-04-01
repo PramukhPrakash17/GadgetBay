4e3ba1ee8c412c8be33d54a41e8607eff1ef3b8f
Pramukh Prakash <pramukhp35@gmail.com>
2025-04-01 21:29:19 +0200

    updating Routes

--- API-Gateway/src/main/java/com/pramukh/shoppingapplication/apigateway/Routes/Routes.java
+++ API-Gateway/src/main/java/com/pramukh/shoppingapplication/apigateway/Routes/Routes.java
@@ -14,6 +14,28 @@ public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
-        return GatewayRouterFunctions.route("product_service").route(RequestPredicates.path("/api/product/create").and(RequestPredicates.method(HttpMethod.POST)), HandlerFunctions.http("http://localhost:8080")).build();
+        return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product/create").and(RequestPredicates.method(HttpMethod.POST)),
+                HandlerFunctions.http("http://localhost:8080"))
+
+                .route(RequestPredicates.path("/api/product/getall").and(RequestPredicates.method(HttpMethod.GET)),
+                HandlerFunctions.http("http://localhost:8080"))
+                .build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> OrderService() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/order/placeorder").and(RequestPredicates.method(HttpMethod.POST)),
+                HandlerFunctions.http("http://localhost:8081")).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> InventoryService() {
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory/checkinventory").and(RequestPredicates.method(HttpMethod.GET)),
+                        HandlerFunctions.http("http://localhost:8082")).build();
+    }
+
+
 }
